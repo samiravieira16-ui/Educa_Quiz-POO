599192798a218c65411ffc1cfbe5bb41b0a7885f
samiravieira16-ui <samiravieira16@gmail.com>
2026-01-16 19:39:27 -0300

    Adicionar método para preparar rodada de quiz com perguntas aleatórias
    Adiciona método de preparação de rodada com questões aleatórias e altera protocolo de iteração.

--- src/models/quiz.py
+++ src/models/quiz.py
@@ -1,3 +1,5 @@
+import random
+
 class Quiz:
     """
     Classe agregadora que compõe um conjunto de perguntas para formar uma avaliação.
@@ -7,15 +9,17 @@ class Quiz:
         self.titulo = titulo
         self._perguntas = []
 
-    def adicionar_pergunta(self, pergunta):
-        """Adiciona um objeto Pergunta à lista interna do quiz."""
-        self._perguntas.append(pergunta)
-
-    def __len__(self):
-        """Permite usar len(quiz) para saber o total de questões."""
-        return len(self._perguntas)
-
-    def __iter__(self):
-        """Permite iterar diretamente sobre o objeto: 'for p in quiz:'"""
-        return iter(self._perguntas)
+     def preparar_rodada(self, quantidade): 
+        """Seleciona N perguntas aleatórias do banco total.""" 
+        if quantidade > len(self._perguntas): 
+            quantidade = len(self._perguntas) 
+        # Embaralha e corta a lista 
+        self._perguntas = random.sample(self._perguntas, quantidade) 
+ 
+    def __len__(self): 
+        return len(self._perguntas) 
+ 
+    def __getitem__(self, index): 
+        """Permite acessar quiz[0] diretamente.""" 
+        return self._perguntas[index]
     
